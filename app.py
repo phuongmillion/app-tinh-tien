15876eb0d3e2bd1847afd3a79149ef0d8cea2b2d
phuongmillion <phuongmillion1@gmail.com>
2018-11-01 15:39:38 +0700

    gan xong roi ne

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ def hello():
                     "tien_lai_10_ngay": nguoi_vay.tien_lai_10_ngay,
                     "sdt": nguoi_vay.sdt,
                     "thanhtien": nguoi_vay.thanhtien,
-                    "tientrongthang": nguoi_vay.tientrongthang,
+                    # "tientrongthang": nguoi_vay.tientrongthang,
                     "tienchualay": nguoi_vay.tienchualay
                 }
                 tong_nguoi.append(nguoi)
@@ -140,7 +140,7 @@ def get_danh_sach():
     return render_template('danhsach.html', tong_nguoi=tong_nguoi, tong=tong_tien, tongchualay=tong_tien_chua_lay)
 
 
-@app.route("/danhsach/", methods=['POST'])
+@app.route("/danhsach", methods=['POST'])
 def dele_danh_sach():
     global tong_tien
     # global tong_tien_trong_thang
@@ -156,13 +156,64 @@ def dele_danh_sach():
         return redirect('/danhsach')
     return redirect('/danhsach')
 
+@app.route("/danhsach/", methods=['POST'])
+def edit_danh_sach():
+    global tong_nguoi
+    # global tong_tien_trong_thang
+    global tong_tien_chua_lay
+    global tong_tien
+    global list_nguoi
+    form = ReusableForm(request.form)
+    if request.method == 'POST':
+        for nguoi in tong_nguoi:
+            if nguoi["name"] == request.form['name']:
+                tong_tien = tong_tien - nguoi["thanhtien"]
+                tong_tien_chua_lay = tong_tien_chua_lay - nguoi["tienchualay"]
+                tong_nguoi.remove(nguoi)
+        split_year_borrow, split_month_borrow, split_date_borrow = request.form['ngayvay'].split("-")
+        split_y_back, split_m_back, split_d_back = request.form['ngaytra'].split("-")
+        split_chua_lay_y, split_chua_lay_m, split_chua_lay_d = request.form['ngaychualay'].split("-")
+        nguoi_vay = NguoiVay(name=request.form['name'],
+                             ngay_muon=datetime.date(int(split_year_borrow), int(split_month_borrow),
+                                                     int(split_date_borrow)),
+                             ngay_ket_thuc=datetime.date(int(split_y_back), int(split_m_back), int(split_d_back)),
+                             tien_lai_10_ngay=int(request.form['tienlai']),
+                             sdt=int(request.form['sdt']),
+                             ngay_chua_lay=datetime.date(int(split_chua_lay_y), int(split_chua_lay_m),
+                                                         int(split_chua_lay_d)))
+
+        if form.validate():
+            # tong_nguoi.append(nguoi_vay)
+            # tong_tien_trong_thang = tong_tien_trong_thang + nguoi_vay.tientrongthang
+            tong_tien = tong_tien + nguoi_vay.thanhtien
+            tong_tien_chua_lay = tong_tien_chua_lay + nguoi_vay.tienchualay
+            with open('./data.json', 'w') as f:
+                nguoi = {
+                    "name": nguoi_vay.name,
+                    "ngay_muon": str(nguoi_vay.ngay_muon),
+                    "ngay_chua_lay": str(nguoi_vay.ngay_chua_lay),
+                    "ngay_ket_thuc": str(nguoi_vay.ngay_ket_thuc),
+                    "tien_lai_10_ngay": nguoi_vay.tien_lai_10_ngay,
+                    "sdt": nguoi_vay.sdt,
+                    "thanhtien": nguoi_vay.thanhtien,
+                    # "tientrongthang": nguoi_vay.tientrongthang,
+                    "tienchualay": nguoi_vay.tienchualay
+                }
+                tong_nguoi.append(nguoi)
+                json.dump(tong_nguoi, f)
+            msg = Message(subject="Hello",
+                          sender=app.config.get("MAIL_USERNAME"),
+                          recipients=["phuongmillion1@gmail.com"])
+            with app.open_resource("data.json") as fp:
+                msg.attach("data.json", "application/json", fp.read())
+            chitaomoibiet.send(msg)
+        else:
+            flash('Error: All the form fields are required. ')
+        return redirect('/danhsach')
 
 @app.route("/contact", methods=['GET'])
 def get_contact():
-    return render_template('Edit.html', tong_nguoi=tong_nguoi, tong=tong_tien, tongchualay=tong_tien_chua_lay)
-
-
-
+    return render_template('contact.html')
 
 if __name__ == "__main__":
     app.config['TEMPLATES_AUTO_RELOAD'] = True
